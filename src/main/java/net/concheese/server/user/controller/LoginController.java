c4172f367893f9b3c1a10bbed83eb3bbf73c3778
Ji Myoung Ha <webmaster@null0xff.com>
2023-10-23 03:21:31 +0900

    :recycle: User -> Login 클래스 이름 변경

--- src/main/java/net/concheese/server/user/controller/LoginController.java
+++ src/main/java/net/concheese/server/user/controller/LoginController.java
@@ -3,20 +3,20 @@ package net.concheese.server.user.controller;
 
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
-import net.concheese.server.user.model.UserSession;
+import net.concheese.server.user.model.LoginSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RequiredArgsConstructor
 @Controller
-public class UserController {
+public class LoginController {
 
   private final HttpSession httpSession;
 
   @GetMapping("/")
   public String index(Model model) {
-    UserSession user = (UserSession) httpSession.getAttribute("user");
+    LoginSession user = (LoginSession) httpSession.getAttribute("user");
     if (user != null) {
       model.addAttribute("id", user.getId());
     }
